29ff1d323f00b2252461a68164902c75d831775d
imurata <imurata@vmware.com>
2022-09-13 13:09:39 +0900

    nocomment

---
+++ src/main/java/com/example/project/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+/**
+ * Calculatorクラスのコメント
+ * 
+ * @author hogehoge
+ */
+public class Calculator {
+
+	/**
+	 * addは引数の和を返します
+	 * 
+	 * @param a 第一引数
+	 * @param b 第二引数
+	 * @return 第一引数と第二引数の和
+	 */
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
